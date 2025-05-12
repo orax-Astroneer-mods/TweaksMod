e3c09ef62c87f6f835ec4df2f7184b7d9437e944
orax <orax@users.noreply.github.com>
2025-05-12 20:39:51 +0200

    remove obsolete code

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -56,23 +56,6 @@ local ESlateVisibility = {
     ESlateVisibility_MAX = 5,
 }
 
----@class Debug
----@field staticMeshActorClassShortName string
----@field staticMeshActorClassName string
----@field staticMeshActorClass UClass?
----@field material UMaterialInterface?
----@field mesh UStaticMesh?
----@field scale FVector
-local debug = {
-    staticMeshActorClassShortName = "StaticMeshActor",
-    staticMeshActorClassName = "/Script/Engine.StaticMeshActor",
-    staticMeshActorClass = nil,
-    material = nil,
-    material2 = nil,
-    mesh = nil,
-    scale = { X = 0.1, Y = 0.1, Z = 0.1 }
-}
-
 local IsAutoWalkEnabled = false
 local MaxSpeed = nil
 local IsFirstInit = true
@@ -94,49 +77,14 @@ local function vec3ToFVector(v)
     return { X = v.x, Y = v.y, Z = v.z }
 end
 
----@param world UWorld
----@param staticMeshActorClass UClass
----@param mesh UStaticMesh
----@param material UMaterialInterface
----@param location FVector
----@param rotation FRotator?
----@param scale FVector?
----@param color FLinearColor?
----@return AStaticMeshActor
-local function spawnDebugObject(world, staticMeshActorClass, mesh, material, location, rotation, scale, color)
-    rotation = rotation or { Pitch = 0, Roll = 0, Yaw = 0 }
-    scale = scale or { X = 1, Y = 1, Z = 1 }
-    color = color or { R = 1.0, G = 1.0, B = 1.0, A = 1.0 }
-
-    ---@diagnostic disable-next-line: undefined-field
-    local staticMeshActor = world:SpawnActor(staticMeshActorClass, fvectorToUserData(location),
-        rotation) ---@cast staticMeshActor AStaticMeshActor
-    assert(staticMeshActor:IsValid())
-
-    staticMeshActor.StaticMeshComponent.StaticMesh = mesh
-
-    local matInstance = staticMeshActor.StaticMeshComponent:CreateDynamicMaterialInstance(0, material, FName(0))
-    matInstance:SetVectorParameterValue(FName("Color"), color)
-
-    local bounds = staticMeshActor.StaticMeshComponent.StaticMesh:GetBounds()
-
-    local newScale = {
-        X = scale.X / bounds.BoxExtent.X,
-        Y = scale.Y / bounds.BoxExtent.Y,
-        Z = scale.Z / bounds.BoxExtent.Z
-    }
-    staticMeshActor:SetActorScale3D(newScale)
-
-    return staticMeshActor
-end
-
 ---@param self RemoteUnrealParam?
 ---@param NewPawn RemoteUnrealParam?
 local function init(self, NewPawn)
     local pc, newPawn
 
-    ---@diagnostic disable-next-line: assign-type-mismatch
-    AstroPlayStatics = StaticFindObject("/Script/Astro.Default__AstroPlayStatics") ---@type UAstroPlayStatics
+    if not AstroPlayStatics:IsValid() then
+        AstroPlayStatics = StaticFindObject("/Script/Astro.Default__AstroPlayStatics") ---@diagnostic disable-line: cast-local-type
+    end
 
     if self then
         pc = self:get()
@@ -152,46 +100,8 @@ local function init(self, NewPawn)
     end
     ---@cast newPawn ADesignAstro_C
 
-    ExecuteInGameThread(function()
-        --[[
-        Open FModel, go in Engine > Content > EngineDebugMaterials
 
-        "/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial"
-        "/Engine/EngineDebugMaterials/WireframeMaterial.WireframeMaterial" -- Params: Color (wireframe, emissive).
-        "/Engine/EngineDebugMaterials/DebugMeshMaterial.DebugMeshMaterial" -- Params: Color (emissive).
-        "/Engine/EngineDebugMaterials/DebugEditorMaterial.DebugEditorMaterial" -- Params: Color, Desaturation, Opacity (emissive).
-        "/Engine/EngineDebugMaterials/M_SimpleTranslucent.M_SimpleTranslucent" -- Params: Color (translucent).
-        "/Engine/EngineMaterials/EmissiveTexturedMaterial.EmissiveTexturedMaterial" -- Params: Texture.
-        "/Engine/EngineMaterials/WorldGridMaterial.WorldGridMaterial" -- Params: None.
-    ]]
-        local mat = "/Engine/EngineDebugMaterials/WireframeMaterial.WireframeMaterial"
-        LoadAsset(mat) ---@diagnostic disable-line: undefined-global
-        local mat2 = "/Engine/EngineDebugMaterials/DebugMeshMaterial.DebugMeshMaterial"
-        LoadAsset(mat2) ---@diagnostic disable-line: undefined-global
 
-        -- Cone, Cube, Cylinder, Plane, Sphere
-        local mesh = "/Engine/BasicShapes/Sphere.Sphere"
-        LoadAsset(mesh) ---@diagnostic disable-line: undefined-global
-
-        debug.staticMeshActorClass = StaticFindObject(debug.staticMeshActorClassName) ---@diagnostic disable-line: assign-type-mismatch
-        debug.material = StaticFindObject(mat) ---@diagnostic disable-line: assign-type-mismatch
-        debug.material2 = StaticFindObject(mat2) ---@diagnostic disable-line: assign-type-mismatch
-        debug.mesh = StaticFindObject(mesh) ---@diagnostic disable-line: assign-type-mismatch
-
-        if not debug.staticMeshActorClass:IsValid() then
-            debug.staticMeshActorClass = StaticFindObject(debug.staticMeshActorClassName) ---@diagnostic disable-line: assign-type-mismatch
-        end
-        if not debug.material:IsValid() then
-            debug.material = StaticFindObject(mat) ---@diagnostic disable-line: assign-type-mismatch
-        end
-        if not debug.mesh:IsValid() then
-            debug.mesh = StaticFindObject(mesh) ---@diagnostic disable-line: assign-type-mismatch
-        end
-
-        if not AstroPlayStatics:IsValid() then
-            AstroPlayStatics = StaticFindObject("/Script/Astro.Default__AstroPlayStatics") ---@diagnostic disable-line: cast-local-type
-        end
-    end)
 
     -- Spot Light
     -- https://dev.epicgames.com/documentation/en-us/unreal-engine/1.2---spot-light?application_version=4.27
@@ -493,9 +403,3 @@ do
 end
 
 init()
-local dbgObjectsInst = FindAllOf(debug.staticMeshActorClassShortName) ---@type AActor[]?
-if dbgObjectsInst then
-    for _, value in ipairs(dbgObjectsInst) do
-        value:K2_DestroyActor()
-    end
-end
